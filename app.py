a3e3f3b5b5cc1b6a629a5ab2aa1ce37564518b32
Shota Minegishi <smngs@mine-313.com>
2022-07-08 19:22:10 +0900

    [Init] Initial commit
    * とりあえず Hello World まで．続きは今度やる．

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# ボットトークンと署名シークレットを使ってアプリを初期化します
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# 'hello' を含むメッセージをリッスンします
+# 指定可能なリスナーのメソッド引数の一覧は以下のモジュールドキュメントを参考にしてください：
+# https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
+    say(f"Hey there <@{message['user']}>!")
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
